25e7824c1239e04538de7a6bd01b50fe6e0db048
Kelly Solano <kelly.solano.1403@gmail.com>
2024-05-23 22:35:48 -0500

    Implementation

--- Algorithms Stanford - Coursera/Semana 2/NumberOfInvertions.py
+++ Algorithms Stanford - Coursera/Semana 2/NumberOfInvertions.py
@@ -1,5 +1,33 @@
+def sort_count_split_inv(array):
+    n = len(array)
+    if n <= 1:
+        return array, 0
+    A, Ac = sort_count_split_inv(array[:n//2])
+    B, Bc = sort_count_split_inv(array[n//2:])
+    count = 0 + Ac + Bc
+    i = 0
+    j = 0
+    C = []
+    while i < len(A) and j < len(B):
+        if A[i] < B[j]:
+            C.append(A[i])
+            i += 1
+        else:
+            C.append(B[j])
+            j += 1
+            count += len(A) - i
+    if i < len(A):
+        C.extend(A[i:])
+    if j < len(B):
+        C.extend(B[j:])
+        
+    return C, count
+
+
 array = []
-with open('/IntegerArray.txt', 'r') as f:
+with open('Algorithms\Algorithms Stanford - Coursera\Semana 2\IntegerArray.txt', 'r') as f:
     for line in f:
-        array.append(int(line))
-print(array)
+        array.append(int(line.strip()))
+
+print(f"Number of invertions: {sort_count_split_inv(array)[1]}")
+
